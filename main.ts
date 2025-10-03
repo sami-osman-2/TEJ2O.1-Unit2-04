97101217efabb3f6ba4fbdd2e26b5175a2d5b2f5
sami-osman-2 <sami.osman@stu.ocsb.ca>
2025-10-03 13:17:31 -0400

    Fixed format

--- main.ts
+++ main.ts
@@ -15,8 +15,9 @@ basic.pause(1000)
 
 // when button A is pressed the temperature is shown
 input.onButtonPressed(Button.A, function () {
+    Temperature = input.temperature()
     basic.clearScreen()
-    basic.showString('The temperature is' + input.temperature())
+    basic.showString('The temperature is' + Temperature)
     basic.clearScreen()
     basic.showIcon(IconNames.Happy)
     basic.pause(1000)
